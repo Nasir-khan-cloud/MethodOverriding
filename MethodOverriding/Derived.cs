fb9df829c089b2b7d27db71ec50eac69a03d04c9
Muka <Muka@DESKTOP-3D662UM>
2023-04-28 09:28:09 +0600

    Add project files.

---
+++ MethodOverriding/Derived.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverriding
+{
+    public class Derived : Base
+    {
+        public override void show()
+        {
+            Console.WriteLine("Derived Class");
+        }
+    }
+}
---
+++ MethodOverriding/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using MethodOverriding;
+
+
+Derived derived = new Derived();
+derived.show();
+
+Base b = new Base();
+b.show();
